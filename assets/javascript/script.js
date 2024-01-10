19fe33f49dbbff8b5adf7f65f9fd0d8967127b20
Aansteydev <>
2024-01-10 11:57:30 +0000

    add all questions 10/10

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,7 +13,7 @@ const questions = [
         optionB: "12",
         optionC: "11",
         optionD: "9",
-        correctOption: "optionB"
+        correctOption: "optionC"
     },
     {
         question: "Which games company is well known for creating the Dark Souls and Armoured Core series of games?",
@@ -32,7 +32,53 @@ const questions = [
         correctOption: "optionA"
     },
     {
-        question: "",
-
+        question: "British games devs Rare had a hit one their hands with Goldeneye for the N64 but what year did they first begin to create games? ",
+        optionA: "1980",
+        optionB: "1984",
+        optionC: "1985",
+        optionD: "1982",
+        correctOption: "optionC"
+    },
+    {
+        question:"Nintendo broke into the market with the Famicom in 1983 but what was the name of the add on for that system?",
+        optionA:"The Gameboy",
+        optionB:"Disk System",
+        optionC:"The Famicom Twin",
+        optionD:"64DD",
+        correctOption:"optionB"
+        
+    },
+    {
+        question: "The Castlevania series is one of Konami's best series of games but what is the name of the protagonist from the first title?",
+        optionA: "Trevor Belmont",
+        optionB: "Alucard",
+        optionC: "Sypha Belnades",
+        optionD: "Simon Belmont",
+        correctOption: "optionD"
+    },
+    {
+        question: "Capcom are well known for their games such as Resident Evil and Street Fighter but what is Capcom and abbreviation of? ",
+        optionA: "Capsule company ",
+        optionB: "Capsule Computer",
+        optionC: "Capsule Com",
+        optionD: "Capsule Community",
+        correctOption: "optionA"
+    },
+    {
+        question: "The Resident Evil series has had a few spin offs over the years but what is the title of the only Gameboy Colour game that was created in the series?",
+        optionA: "Resident Evil 2",
+        optionB: "Resident Evil Gaiden",
+        optionC: "Resident Evil Deadly Silence",
+        optionD: "Resident Evil Portable",
+        correctOption: "optionB"
+    },
+    {
+        question: "Which manga artist has famously created art for the Dragon Quest series of games? ",
+        optionA: "Akira Toriyama",
+        optionB: "Katsuhiro Otomo",
+        optionC: "Naoki Urasawa",
+        optionD: "Rumiko Takahashi",
+        correctOption: "optionA"
     },
 ];
+
