3e625314c39b7a43d2eb19c74edc65a27e530ebe
Aansteydev <>
2024-01-10 11:29:14 +0000

    add inital files/folders began writing quiz q+a

---
+++ assets/javascript/script.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "When was the first Super Mario Brothers game released?",
+        optionA: "1983",
+        optionB: "1985",
+        optionC: "1988",
+        optionD: "1980",
+        correctOption: "optionA"
+    },
+    {
+        question: "How many games are in the original Megaman series?",
+        optionA: "10",
+        optionB: "12",
+        optionC: "11",
+        optionD: "9",
+        correctOption: "optionB"
+    },
+    {
+        question: "Which games company is well known for creating the Dark Souls and Armoured Core series of games?",
+        OptionA: "Konami",
+        optionB: "Capcom",
+        optionC: "Nintendo",
+        optionD: "From Software",
+        correctOption: "optionD"
+    },
+    {
+        question: "What was the name of Sega's first games console?",
+        optionA: "The SG-1000",
+        optionB: "The Master System",
+        optionC: "The Megadrive",
+        optionD: "The Sega Saturn",
+        correctOption: "optionA"
+    },
+    {
+        question: "",
+
+    },
+];
