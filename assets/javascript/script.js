c90035439d4eeabf66b5f26f10a44931e5e64dc7
Aansteydev <>
2024-01-15 10:02:12 +0000

    add 10 more questions to question array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -32,7 +32,7 @@ const questions = [
         correctOption: "optionA"
     },
     {
-        question: "British games devs Rare had a hit one their hands with Goldeneye for the N64 but what year did they first begin to create games? ",
+        question: "British games devs Rare had a hit on their hands with Goldeneye for the N64 but what year did they first begin to create games? ",
         optionA: "1980",
         optionB: "1984",
         optionC: "1985",
@@ -40,13 +40,13 @@ const questions = [
         correctOption: "optionC"
     },
     {
-        question:"Nintendo broke into the market with the Famicom in 1983 but what was the name of the add on for that system?",
-        optionA:"The Gameboy",
-        optionB:"Disk System",
-        optionC:"The Famicom Twin",
-        optionD:"64DD",
-        correctOption:"optionB"
-        
+        question: "Nintendo broke into the market with the Famicom in 1983 but what was the name of the add on for that system?",
+        optionA: "The Gameboy",
+        optionB: "Disk System",
+        optionC: "The Famicom Twin",
+        optionD: "64DD",
+        correctOption: "optionB"
+
     },
     {
         question: "The Castlevania series is one of Konami's best series of games but what is the name of the protagonist from the first title?",
@@ -78,6 +78,87 @@ const questions = [
         optionB: "Katsuhiro Otomo",
         optionC: "Naoki Urasawa",
         optionD: "Rumiko Takahashi",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "In what game did Sonic the Hedgehog have his first debut?",
+        optionA:"Sonic the Hedgehog",
+        optionB:"Rad Racer",
+        optionC:"Rad Mobile",
+        optionD:"Fighting Vipers",
+        correctOption:"optionC"
+    },
+    {
+        question: "Nintendo were founded in 1889 in Kyoto, Japan what were they famous for making at the time?",
+        optionA: "Pachislot",
+        optionB: "Vending machines",
+        optionC: "Alcohol",
+        optionD: "Playing cards",
+        correctOption: "optionD"
+    },
+    {
+        question: " In what year did Wario, Marios nemesis make his debut?",
+        optionA:"1992",
+        optionB:"1985",
+        optionC:"1993",
+        optionD:"1983",
+        correctOption:"optionA"
+    },
+    {
+        question: "Sega had a hit on it's hands with 'Nights into Dreams' for the Sega Saturn but who produced the game?",
+        optionA: "Rieko Kodama",
+        optionB: "Yu Suzuki",
+        optionC: "Yuji Saito",
+        optionD: "Yuji Naka",
+        correctOption: "optionB"
+    },
+    {
+        question: "Final Fantasy 7(1997) featured a summon that took ages to finish their attack, what is their name?",
+        optionA: "Neo Bahamut",
+        optionB: "Odin",
+        optionC: "Ifrit",
+        optionD: "Knights of the round",
+        correctOption: "optionD"
+    },
+    {
+        question: "Pikmin was a hit on the Nintendo Gamecube but what type of game is it?",
+        optionA: "Fishing simulator",
+        optionB: "Real time strategy",
+        optionC: "Platformer",
+        optionD: "Beat 'em up",
+        correctOption: "optionB"
+    },
+    {
+        question: " Every game in the Final Fantasy series contains a character with which name?",
+        optionA: "Locke",
+        optionB: "Cid",
+        optionC: "Kefka",
+        optionD: "Garland",
+        correctOption: "optionB"
+    },
+    {
+        question: " Captain Falcon originally appeared in what game?",
+        optionA:"Earthbound",
+        optionB:"Pilot Wings",
+        optionC:"F-Zero",
+        optionD:"Star Fox",
+        correctOption:"optionC"
+    },
+    {
+        question: "Earthbound is a classic JRPG, what is the name of the series creator? ",
+        optionA: "Shigeru Miyamoto",
+        optionB: "Shigesato Itoi",
+        optionC: "Hironobu Sakaguchi",
+        optionD: "Yu Suzuki",
+        correctOption: "optionB"
+    },
+    {
+        question: "Donkey Kong JR is a spin off of the arcade classic which had it's own spin off which taught...?",
+        optionA: "Maths",
+        optionB: "English",
+        optionC: "Science",
+        optionD: "Philosophy",
         correctOption: "optionA"
     },
 ];
